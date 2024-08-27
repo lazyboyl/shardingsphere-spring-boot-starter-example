93b549949bf974310661667ca26895499fdb1ce4
linzf <282245889@qq.com>
2024-08-27 16:36:52 +0800

    加密配置修改

---
+++ sharding-algorithm/src/main/java/com/lazyboyl/sharding/algorithm/ShardingAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.lazyboyl.sharding.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingAlgorithmApplication.class, args);
+    }
+
+}
