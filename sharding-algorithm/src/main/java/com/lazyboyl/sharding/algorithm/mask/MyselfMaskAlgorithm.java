a1e44bac86fe32b1e918af09fcea3eaffcf8e20e
linzf <282245889@qq.com>
2024-08-30 16:14:44 +0800

    配置修改

---
+++ sharding-algorithm/src/main/java/com/lazyboyl/sharding/algorithm/mask/MyselfMaskAlgorithm.java
@@ -0,0 +1,30 @@
+package com.lazyboyl.sharding.algorithm.mask;
+
+import org.apache.shardingsphere.mask.spi.MaskAlgorithm;
+
+import java.util.Properties;
+
+/**
+ * 自定义脱敏实现
+ * @author linzf
+ */
+public class MyselfMaskAlgorithm implements MaskAlgorithm<Object, String> {
+
+    private Properties props;
+
+    @Override
+    public String mask(Object o) {
+        String s = (String) o;
+        return s.substring(0,1) + "*****";
+    }
+
+    @Override
+    public void init(Properties props) {
+        this.props = props;
+    }
+
+    @Override
+    public String getType() {
+        return "MyselfMask";
+    }
+}
